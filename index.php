b4e2f914183f27bd98567d8fdec2e42b62958255
Akhwand Abdul Kareem <akhwandabdulkareem@gmail.com>
2019-07-18 19:59:00 +0500

    First commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <!-- Bootstrap 4 CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+        <title>Home Page</title>
+
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 offset-md-4 form-div login">
+
+                    <div class="alert alert-success">
+                    You are now logged in!
+                    </div>
+
+                    <h3>Welcome Awa</h3>
+
+                    <a href="#" class="logout">Logout</a>
+
+                    <div class="alert alert-warning">
+                     You need to verify your account.
+                     Sign in to your account and click on the
+                     verification link we just emailed at
+                     <strong>akhwandabdulkareem@gmail.com</strong>
+                    </div>
+                    <button class="btn btn-block btn-lg btn-primary">I am verified!</button>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
